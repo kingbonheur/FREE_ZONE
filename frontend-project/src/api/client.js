2e8cd8cfbb86befa0a9d2e589d43da992db4ce99
King Bonheur <kingbonheur.kb@gmail.com>
2026-06-22 10:51:00 -0700

    Saved

--- frontend-project/src/api/client.js
+++ frontend-project/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const API_URL = "http://freezonebar-api.vercel.app" || import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
   baseURL: `${API_URL}/api`,
